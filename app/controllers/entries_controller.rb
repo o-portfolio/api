dd95eaf65f24a6fa6542273eb0503b807724eed5
Jeremy Walker <jeremy@meducation.net>
2013-01-26 23:46:48 +0000

    Fully working API

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,8 @@
 class EntriesController < ApplicationController
   
   def index
+    @entries = @current_user.entries
+    render json: @entries
   end
   
   def show
@@ -9,14 +11,14 @@ class EntriesController < ApplicationController
   end
   
   def create
-    @entry = @current_user.entries.create(params.slice(:title, :description, :reflection))
+    @entry = @current_user.entries.create(params.slice(:title, :description, :reflection, :occurred_at))
     render json: @entry
   end
   
   def update
     @entry = @current_user.entries.find(params[:id])
-    @entry.update_attributes(params.slice(:title, :description, :reflection))
-    render json: user_to_json(@current_user)
+    @entry.update_attributes(params.slice(:title, :description, :reflection, :occurred_at))
+    render json: @entry
   end
   
   def destroy
